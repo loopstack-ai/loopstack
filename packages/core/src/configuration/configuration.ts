d629bd03cd6a52031c880dd1857233aa2742f21a
Jakob <jakob.klippel@config.one>
2025-02-20 10:51:57 +0100

    move schemas to shared module

--- packages/core/src/configuration/configuration.ts
+++ packages/core/src/configuration/configuration.ts
@@ -1,25 +1,6 @@
-import * as fs from 'fs';
-import * as path from 'path';
-import {Schema} from "ajv";
+import { loadLoopstackSchemas } from '@loopstack/shared';
 
 export default () => {
-    const schemasDir = path.join(__dirname, 'schemas');
-    const schemaFiles = fs.readdirSync(schemasDir).filter(file => file.endsWith('schema.json'));
-
-    const schemas: Schema[] = [];
-    let mainSchema: Schema | undefined;
-
-    for (const file of schemaFiles) {
-        const filePath = path.join(schemasDir, file);
-        // const key = file.replace('.schema.json', ''); // Use filename (without extension) as key
-        const schema = JSON.parse(fs.readFileSync(filePath, 'utf-8'));
-        // console.log(schema)
-        if (schema['$id'] === 'https://loopstack.ai/schemas/main.schema.json') {
-            mainSchema = schema;
-            continue;
-        }
-        schemas.push(schema);
-    }
-
-    return { mainSchema, schemas };
+    const schemasConfig = loadLoopstackSchemas();
+    return { ...schemasConfig };
 };
