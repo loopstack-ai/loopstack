173ffeb7f59a90444ee92d9d97a867ec2e4d3ec4
Jakob <jakob.klippel@config.one>
2025-02-28 11:33:24 +0100

    add indexes

--- packages/core/src/index.ts
+++ packages/core/src/index.ts
@@ -1,3 +1,6 @@
 export * from './loop-core.module';
 export * from './configuration/services/init.service';
 export * from './utils';
+export * from './persistence/entities';
+export * from './persistence/interfaces';
+export * from './processor/interfaces';
---
+++ packages/core/src/persistence/interfaces/index.ts
@@ -0,0 +1,1 @@
+export * from './history-transition.interface';
