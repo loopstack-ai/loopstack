8d915c4088b5dfb98058856f3bfdcbcef7a0f090
Jakob <jakob.klippel@config.one>
2025-06-04 13:53:07 +0200

    feat: improve ports for local development env

--- templates/app-template/src/main.ts
+++ templates/app-template/src/main.ts
@@ -10,6 +10,6 @@ async function bootstrap() {
 
   LoopstackApiModule.setup(app as any);
 
-  await app.listen(process.env.PORT ?? 3000);
+  await app.listen(process.env.PORT ?? 8000);
 }
 bootstrap();
