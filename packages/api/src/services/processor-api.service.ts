f1d68573338794597de18b4cf691ed0b205b37cf
Jakob <jakob.klippel@config.one>
2025-07-29 16:47:41 +0200

    feat: execute manual pipeline run as task

--- packages/api/src/services/processor-api.service.ts
+++ packages/api/src/services/processor-api.service.ts
@@ -1,50 +1,34 @@
 import {
-  ConflictException,
   Injectable,
-  NotFoundException,
 } from '@nestjs/common';
-import { InjectRepository } from '@nestjs/typeorm';
-import { IsNull, Repository } from 'typeorm';
 import { RunPipelinePayloadDto } from '../dtos/run-pipeline-payload.dto';
 import {
-  PipelineEntity,
+  ScheduledTask,
 } from '@loopstack/shared';
-import { RootProcessorService } from '@loopstack/core/dist/modules/workflow-processor/services/root-processor.service';
+import { TaskSchedulerService } from '@loopstack/core/dist/modules/scheduler';
 
 @Injectable()
 export class ProcessorApiService {
   constructor(
-    @InjectRepository(PipelineEntity)
-    private pipelineEntityRepository: Repository<PipelineEntity>,
-    private rootProcessorService: RootProcessorService,
+    private taskSchedulerService: TaskSchedulerService,
   ) {}
 
   async processPipeline(
     pipelineId: string,
     user: string | null,
     payload: RunPipelinePayloadDto,
-    options?: {
-      force?: boolean;
-    },
   ): Promise<any> {
-    const pipeline = await this.pipelineEntityRepository.findOne({
-      where: {
-        id: pipelineId,
-        createdBy: user === null ? IsNull() : user,
+    return this.taskSchedulerService.addTask({
+      id: 'manual_pipeline_execution',
+      task: {
+        name: 'manual_execution',
+        type: 'run_pipeline',
+        payload: {
+          ...payload,
+          id: pipelineId,
+        },
+        user: user
       },
-      relations: ['workspace'],
-    });
-
-    if (!pipeline) {
-      throw new NotFoundException(`Pipeline with id ${pipelineId} not found.`);
-    }
-
-    if (pipeline.workspace.isLocked && !options?.force) {
-      throw new ConflictException(
-        `Pipeline with id ${pipelineId} is locked by another process. User force = true to override.`,
-      );
-    }
-
-    return this.rootProcessorService.runPipeline(pipeline, payload, options);
+    } satisfies ScheduledTask);
   }
 }
