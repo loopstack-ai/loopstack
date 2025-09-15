c50bdd508ac7c62c7edd5fa3b0a75c7e5269f21f
Jakob <jakob.klippel@config.one>
2025-09-15 14:48:59 +0200

    feat: (wip) use central platform for authentication

--- templates/app-template/src/auth.config.ts
+++ templates/app-template/src/auth.config.ts
@@ -2,11 +2,14 @@ import { AuthStrategy } from '@loopstack/shared';
 import { AuthConfig } from '@loopstack/auth';
 
 export const authConfig: AuthConfig = {
-  strategies: [AuthStrategy.JWT, AuthStrategy.DEV],
+  strategies: [AuthStrategy.JWT, AuthStrategy.HUB],
   jwt: {
     secret: process.env.JWT_SECRET ?? 'NO SECRET',
     expiresIn: '1h',
     refreshSecret: process.env.JWT_SECRET ?? 'NO SECRET',
     refreshExpiresIn: '7d',
   },
+  clientId: process.env.CLIENT_ID,
+  clientSecret: process.env.CLIENT_SECRET,
+  authCallback: process.env.AUTH_CALLBACK_URL || "https://studio.loopstack.ai/api/v1/sso/validate"
 };
--- templates/app-template/src/main.ts
+++ templates/app-template/src/main.ts
@@ -3,10 +3,7 @@ import { AppModule } from './app.module';
 import { LoopstackApiModule } from '@loopstack/api';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule, {
-    logger: ['error', 'warn', 'debug'],
-    abortOnError: false,
-  });
+  const app = await NestFactory.create(AppModule);
 
   LoopstackApiModule.setup(app as any);
 
