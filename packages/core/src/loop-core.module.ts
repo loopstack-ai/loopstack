ad1fa59671b07be69fe721477646baa04c5d0fd0
Jakob <jakob.klippel@config.one>
2025-09-17 11:26:53 +0200

    fix: use single config module from main app

--- packages/core/src/loop-core.module.ts
+++ packages/core/src/loop-core.module.ts
@@ -1,9 +1,4 @@
-import { Module, DynamicModule } from '@nestjs/common';
-import { ConfigModule } from '@nestjs/config';
-import {
-  ConfigurableModuleClass,
-  LoopCoreModuleOptions,
-} from './loop-core.module-definition';
+import { Module } from '@nestjs/common';
 import {
   CommonModule,
   WorkflowProcessorModule,
@@ -16,48 +11,31 @@ import { SchedulerModule } from './modules/scheduler';
 import { EventEmitterModule } from '@nestjs/event-emitter';
 import { MODULE_NAME_TOKEN } from '@loopstack/shared';
 
-@Module({})
-export class LoopCoreModule extends ConfigurableModuleClass {
-  static forRoot(options?: LoopCoreModuleOptions): DynamicModule {
-    return {
-      ...super.register(options ?? {}),
-      imports: [
-        ConfigModule.forRoot({
-          isGlobal: true,
-          load: [
-            () => ({
-              runStartupTasks:
-                undefined === options?.runStartupTasks
-                  ? true
-                  : options.runStartupTasks,
-              configs: options?.configs ?? [],
-            }),
-          ],
-        }),
-        EventEmitterModule.forRoot({
-          global: true,
-        }),
-        CommonModule,
-        ConfigurationModule,
-        PersistenceModule,
-        WorkflowProcessorModule,
-        SchedulerModule,
-      ],
-      providers: [
-        {
-          provide: MODULE_NAME_TOKEN,
-          useValue: 'core',
-        },
-        MigrationsService,
-        ConfigProviderService,
-      ],
-      exports: [
-        ConfigurationModule,
-        CommonModule,
-        PersistenceModule,
-        WorkflowProcessorModule,
-        SchedulerModule,
-      ],
-    };
-  }
-}
+@Module({
+  imports: [
+    EventEmitterModule.forRoot({
+      global: true,
+    }),
+    CommonModule,
+    ConfigurationModule,
+    PersistenceModule,
+    WorkflowProcessorModule,
+    SchedulerModule,
+  ],
+  providers: [
+    {
+      provide: MODULE_NAME_TOKEN,
+      useValue: 'core',
+    },
+    MigrationsService,
+    ConfigProviderService,
+  ],
+  exports: [
+    ConfigurationModule,
+    CommonModule,
+    PersistenceModule,
+    WorkflowProcessorModule,
+    SchedulerModule,
+  ],
+})
+export class LoopCoreModule {}
