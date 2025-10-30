f04f502747fc004d697e7eadebf029b0bdce439d
Jakob <jakob.klippel@config.one>
2025-10-30 11:48:48 +0100

    feat: set document place to the transition to place instead of from

--- packages/core/src/persistence/services/document.service.ts
+++ packages/core/src/persistence/services/document.service.ts
@@ -113,12 +113,17 @@ export class DocumentService {
       throw new Error(`No transition assigned to processor state.`);
     }
 
+    // todo. this check could be done earlier or introduce a specific transition for alternative paths
+    if (Array.isArray(block.ctx.workflow.transition.to)) {
+      throw new Error(`Cannot create a document on an undecided transition target. Make sure to set a singular "to" place for a transition where documents are created`);
+    }
+
     return this.documentRepository.create({
       ...data,
       transition: block.ctx.workflow.transition.id,
       index: block.ctx.workflow.documents?.length ?? 0,
       workflowIndex: block.ctx.index,
-      place: block.ctx.workflow.place,
+      place: block.ctx.workflow.transition.to,
       labels: block.ctx.labels,
       workflow: { id: block.ctx.workflow.id } as WorkflowEntity,
       workspaceId: block.ctx.workspaceId,
