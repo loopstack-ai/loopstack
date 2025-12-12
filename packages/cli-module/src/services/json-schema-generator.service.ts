ae666b4627942855380cb5020c820dbce1c7dc77
Jakob <jakob.klippel@config.one>
2025-12-12 16:58:09 +0100

    first commit

---
+++ packages/cli-module/src/services/json-schema-generator.service.ts
@@ -0,0 +1,32 @@
+import { Injectable } from '@nestjs/common';
+import * as fs from 'fs';
+import * as path from 'path';
+import { zodToJsonSchema } from 'zod-to-json-schema';
+import { writeFileSync } from 'fs';
+// import { DynamicSchemaGeneratorService } from './dynamic-schema-generator.service';
+import { BlockConfigSchema } from '@loopstack/contracts/schemas';
+
+const SCHEMA_PATH = './schema.json';
+
+@Injectable()
+export class JsonSchemaGeneratorService {
+  // constructor(
+  //   private readonly dynamicSchemaGeneratorService: DynamicSchemaGeneratorService,
+  // ) {}
+
+  async generateSchemas() {
+    const outputDir = path.resolve(process.cwd(), 'src/generated');
+    if (!fs.existsSync(outputDir)) {
+      fs.mkdirSync(outputDir, { recursive: true });
+    }
+
+    // const mainSchema = this.dynamicSchemaGeneratorService.getSchema();
+
+    const jsonSchema = zodToJsonSchema(BlockConfigSchema as any, {
+      name: 'MainSchema',
+      target: 'jsonSchema7',
+    });
+
+    writeFileSync(SCHEMA_PATH, JSON.stringify(jsonSchema, null, 2));
+  }
+}
