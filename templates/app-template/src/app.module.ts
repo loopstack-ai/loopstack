34fd5e99b42d2836a5d4b87d2473c3fd4b2aef98
Jakob <jakob.klippel@config.one>
2025-12-15 17:23:08 +0100

    feat: remove example module

--- templates/app-template/src/app.module.ts
+++ templates/app-template/src/app.module.ts
@@ -4,7 +4,6 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { LoopstackApiModule } from '@loopstack/api';
 import { ConfigModule, ConfigService } from '@nestjs/config';
 import { loopstackConfig } from './loopstack.config';
-import { ExampleModule } from './example-module/example.module';
 
 @Module({
   imports: [
@@ -28,7 +27,6 @@ import { ExampleModule } from './example-module/example.module';
     }),
     LoopCoreModule,
     LoopstackApiModule,
-    ExampleModule,
   ],
 })
 export class AppModule {}
