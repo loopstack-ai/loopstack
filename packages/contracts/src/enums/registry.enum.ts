d384b78271699eecbbddb6dddca46e6e9592770d
Tobias Blaettermann <tobias.blaettermann1@gmail.com>
2026-01-12 14:58:31 +0100

    refactor: registry categories

--- packages/contracts/src/enums/registry.enum.ts
+++ packages/contracts/src/enums/registry.enum.ts
@@ -6,25 +6,19 @@ export enum RegistryEntryStatus {
 }
 
 export enum RegistryEntryCategory {
-  FULL_AUTOMATIONS = 'full-automations',
   TOOLS = 'tools',
-  WORKFLOWS = 'workflows',
-  DEMOS_AND_TESTS = 'demos-and-tests',
-  EXPERIMENTAL = 'experimental',
-  STARTER_TEMPLATES = 'starter-templates',
+  EXAMPLES = 'examples',
+  BUNDLES = 'bundles',
+  MODULES = 'modules',
   INTEGRATIONS = 'integrations',
-  AGENTS = 'agents',
 }
 
 export type RegistryCategory = `${RegistryEntryCategory}`;
 
 export const REGISTRY_CATEGORIES: RegistryCategory[] = [
-  RegistryEntryCategory.FULL_AUTOMATIONS,
   RegistryEntryCategory.TOOLS,
-  RegistryEntryCategory.WORKFLOWS,
-  RegistryEntryCategory.DEMOS_AND_TESTS,
-  RegistryEntryCategory.EXPERIMENTAL,
-  RegistryEntryCategory.STARTER_TEMPLATES,
+  RegistryEntryCategory.EXAMPLES,
+  RegistryEntryCategory.BUNDLES,
+  RegistryEntryCategory.MODULES,
   RegistryEntryCategory.INTEGRATIONS,
-  RegistryEntryCategory.AGENTS,
 ];
