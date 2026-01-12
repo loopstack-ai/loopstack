c7d86ac6e09e4a87ba3e5602a5285029fc05733b
Jakob <jakob.klippel@config.one>
2026-01-12 15:06:00 +0100

    chore: update license text

--- registry/tools/create-value-tool/src/create-value-tool.module.ts
+++ registry/tools/create-value-tool/src/create-value-tool.module.ts
@@ -1,5 +1,5 @@
 /*
-Copyright 2025 Jakob Klippel.
+Copyright 2025 The Loopstack Authors.
 
 Licensed under the Apache License, Version 2.0 (the "License");
 you may not use this file except in compliance with the License.
