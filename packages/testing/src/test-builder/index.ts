db1dbeabf4145455926ec26b3ab62dd47c948cab
Jakob <jakob.klippel@config.one>
2026-01-21 08:53:08 +0100

    Remove directory: packages/testing

--- packages/testing/src/test-builder/index.ts
+++
@@ -1,6 +0,0 @@
-export * from './core-module-mock';
-export * from './workflow-test-builder';
-export * from './create-testing-module';
-export * from './create-document-mock';
-export * from './get-tool-result';
-export * from './tool-test-builder';
