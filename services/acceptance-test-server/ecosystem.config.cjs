e37f487730807714e420c9f6e2c1bb7eb2af2788
Tobias Blaettermann <tobias.blaettermann1@gmail.com>
2026-06-09 16:41:29 +0200

    wip

--- services/acceptance-test-server/ecosystem.config.cjs
+++ services/acceptance-test-server/ecosystem.config.cjs
@@ -6,8 +6,6 @@ module.exports = {
       script: 'dist/main.js',
       cwd: (process.env.WORKSPACE_ROOT || '/workspace') + '/app',
       env: { PORT: '3000', HOST: '0.0.0.0', NODE_ENV: 'development' },
-      wait_ready: true,
-      listen_timeout: 10000,
       kill_timeout: 5000,
       merge_logs: true,
     },
